f9f5b30f62f25fbc9df87b21ff4f52af202eb138
daeviz15 <daeviz15felix@gmail.com>
2026-06-20 16:23:46 +0100

    changed the coordinates

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,9 @@
 // Centralized geofencing parameters for the app
 // Using the temporary test coordinates provided by the user (Saheed Bankole St)
-export const TARGET_LAT = 6.429448816210825;
-export const TARGET_LNG = 3.514577921597604;
+export const TARGET_LAT = 6.436922040008514;
+export const TARGET_LNG = 3.5185624946131786;
 export const MAX_DISTANCE_METERS = 100;
 
 // Attendance history pagination
 export const HISTORY_PAGE_SIZE = 7;
+
